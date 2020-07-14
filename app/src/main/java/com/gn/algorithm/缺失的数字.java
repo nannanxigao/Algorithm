d7765cb1a70fe968ce67539eb2def911982b8459
GaoNan <nannanxigao@163.com>
2020-07-14 11:47:08 +0800

    缺失的数字 二分法解

--- app/src/main/java/com/gn/algorithm/缺失的数字.java
+++ app/src/main/java/com/gn/algorithm/缺失的数字.java
@@ -16,7 +16,7 @@ public class 缺失的数字 {
 
     public static void main(String[] args) {
         int[] nums = {1};
-        System.out.println("result:" + missingNumber(nums));
+        System.out.println("result:" + missingNumber1(nums));
     }
 
     /**
@@ -41,6 +41,17 @@ public class 缺失的数字 {
         }
         return result;
     }
+
+    public static int missingNumber1(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int m = (i + j) / 2;
+            if (nums[m] == m) i = m + 1;
+            else j = m - 1;
+        }
+        return i;
+    }
+
 
 
 }
